684314d800a7f5502593c8c3ae98bafd4a8c67de
whoop06 <whoop06@d7f36673-e84d-0410-9a0a-3955734f1af1>
2008-10-29 21:07:14 +0000

    version 0.4.0

--- trunk/lib/jquery.scrollfollow.js
+++ trunk/lib/jquery.scrollfollow.js
@@ -7,19 +7,20 @@
  *
  * @projectDescription	jQuery plugin for allowing an element to animate down as the user scrolls the page.
  * 
- * @version 0.3.1
+ * @version 0.4.0
  * 
  * @requires jquery.js (tested with 1.2.6)
- * @requires ui.core.js (tested with 1.6)
- * @requires jquery.easing.js (http://gsgd.co.uk/sandbox/jquery/easing/ - tested with 1.3)
- * @requires jquery.cookie.js (http://www.stilbuero.de/2006/09/17/cookie-plugin-for-jquery/)
+ * @requires ui.core.js (tested with 1.5.2)
+ * 
+ * @optional jquery.cookie.js (http://www.stilbuero.de/2006/09/17/cookie-plugin-for-jquery/)
+ * @optional jquery.easing.js (http://gsgd.co.uk/sandbox/jquery/easing/ - tested with 1.3)
  * 
  * @param speed		int - Duration of animation (in milliseconds)
  * 								default: 500
  * @param offset			int - Number of pixels box should remain from top of viewport
  * 								default: 0
- * @param easing		string - Any one of the easing options from the easing plugin - < http://gsgd.co.uk/sandbox/jquery/easing/ > - Use 'linear' for no easing
- * 								default: 'easeOutBack'
+ * @param easing		string - Any one of the easing options from the easing plugin - Requires jQuery Easing Plugin < http://gsgd.co.uk/sandbox/jquery/easing/ >
+ * 								default: 'linear'
  * @param container	string - ID of the containing div
  * 								default: box's immediate parent
  * @param killSwitch	string - ID of the On/Off toggle element
@@ -28,10 +29,10 @@
  * 								default: 'Turn Slide Off'
  * @param offText		string - killSwitch text to be displayed if sliding is disabled
  * 								default: 'Turn Slide On'
- * @param animate		string - Setting to 'no' enables a non-animated scroll following action
- * 								default: 'yes
  * @param relativeTo	string - Scroll animation can be relative to either the 'top' or 'bottom' of the viewport
  * 								default: 'top'
+ * @param delay			int - Time between the end of the scroll and the beginning of the animation in milliseconds
+ * 								default: 0
  */
 
 ( function( $ ) {
@@ -44,20 +45,18 @@
 		// 'box' is the object to be animated
 		var position = box.css( 'position' );
 		
-		function ani( box, iTop, iBoxOTop, padAdj )
+		function ani()
 		{		
 			// The script runs on every scroll which really means many times during a scroll.
 			// We don't want multiple slides to queue up.
-			box.dequeue();
-					
-			var docHeight = parseInt( $( document ).height() );
+			box.queue( [ ] );
+		
+			// A bunch of values we need to determine where to animate to
 			var viewportHeight = parseInt( $( window ).height() );	
 			var pageScroll =  parseInt( $( document ).scrollTop() );
-			var parentTop =  parseInt( cont.offset().top );
-			var parentHeight = parseInt( cont.attr( 'offsetHeight' ) );
-			var boxTop =  parseInt( box.offset().top );
+			var parentTop =  parseInt( box.cont.offset().top );
+			var parentHeight = parseInt( box.cont.attr( 'offsetHeight' ) );
 			var boxHeight = parseInt( box.attr( 'offsetHeight' ) + ( parseInt( box.css( 'marginTop' ) ) || 0 ) + ( parseInt( box.css( 'marginBottom' ) ) || 0 ) );
-		
 			var aniTop;
 			
 			// Make sure the user wants the animation to happen
@@ -67,22 +66,22 @@
 				if ( options.relativeTo == 'top' )
 				{
 					// Don't animate until the top of the window is close enough to the top of the box
-					if ( iBoxOTop >= ( pageScroll + options.offset ) )
+					if ( box.initialOffsetTop >= ( pageScroll + options.offset ) )
 					{
-						aniTop = iTop;
+						aniTop = box.initialTop;
 					}
 					else
 					{
-						aniTop = Math.min( ( Math.max( ( -parentTop ), ( pageScroll - iBoxOTop + iTop ) ) + options.offset ), ( parentHeight - boxHeight - padAdj ) );
+						aniTop = Math.min( ( Math.max( ( -parentTop ), ( pageScroll - box.initialOffsetTop + box.initialTop ) ) + options.offset ), ( parentHeight - boxHeight - box.paddingAdjustment ) );
 					}
 				}
 				// If the box should animate relative to the bottom of the window
 				else if ( options.relativeTo == 'bottom' )
 				{
 					// Don't animate until the bottom of the window is close enough to the bottom of the box
-					if ( ( iBoxOTop + boxHeight ) >= ( pageScroll + options.offset + viewportHeight ) )
+					if ( ( box.initialOffsetTop + boxHeight ) >= ( pageScroll + options.offset + viewportHeight ) )
 					{
-						aniTop = iTop;
+						aniTop = box.initialTop;
 					}
 					else
 					{
@@ -90,31 +89,14 @@
 					}
 				}
 				
-				// First options is for modern browsers
-				if ( options.animate == 'yes' )
+				// Checks to see if the relevant scroll was the last one
+				// "-20" is to account for inaccuracy in the timeout
+				if ( ( new Date().getTime() - box.lastScroll ) >= ( options.delay - 20 ) )
 				{
 					box.animate(
 						{
 							top: aniTop
 						}, options.speed, options.easing
-					);
-					
-				}
-				else if ( typeof document.body.style.maxHeight != "undefined" )
-				{
-					box.css(
-						{
-							top: options.offset,
-							position: 'fixed'
-						}
-					);
-				}
-				else
-				{
-					box.css(
-						{
-							top: aniTop
-						}
 					);
 				}
 			}
@@ -123,119 +105,123 @@
 		// For user-initiated stopping of the slide
 		var isActive = true;
 		
-		if( $.cookie( 'scrollFollowSetting' + box.attr( 'id' ) ) == 'false' )
+		if ( $.cookie != undefined )
 		{
-			var isActive = false;
-			
-			$( '#' + options.killSwitch ).text( options.offText )
-				.toggle( 
-					function ()
-					{
-						isActive = true;
-						
-						$( this ).text( options.onText );
-						
-						$.cookie( 'scrollFollowSetting' + box.attr( 'id' ), true, { expires: 365, path: '/'} );
-						
-						ani( box, initialTop, boxInitialOffsetTop, paddingAdjustment );
-					},
-					function ()
-					{
-						isActive = false;
-						
-						$( this ).text( options.offText );
-						
-						if( options.animate == 'yes' )
+			if( $.cookie( 'scrollFollowSetting' + box.attr( 'id' ) ) == 'false' )
+			{
+				var isActive = false;
+				
+				$( '#' + options.killSwitch ).text( options.offText )
+					.toggle( 
+						function ()
 						{
+							isActive = true;
+							
+							$( this ).text( options.onText );
+							
+							$.cookie( 'scrollFollowSetting' + box.attr( 'id' ), true, { expires: 365, path: '/'} );
+							
+							ani();
+						},
+						function ()
+						{
+							isActive = false;
+							
+							$( this ).text( options.offText );
+							
 							box.animate(
 								{
-									top: initialTop
+									top: box.initialTop
 								}, options.speed, options.easing
 							);	
+							
+							$.cookie( 'scrollFollowSetting' + box.attr( 'id' ), false, { expires: 365, path: '/'} );
 						}
-						else
+					);
+			}
+			else
+			{
+				$( '#' + options.killSwitch ).text( options.onText )
+					.toggle( 
+						function ()
 						{
-							box.css(
+							isActive = false;
+							
+							$( this ).text( options.offText );
+							
+							box.animate(
 								{
-									position: position,
-									top: initialTop
-								}
-							);
+									top: box.initialTop
+								}, 0
+							);	
+							
+							$.cookie( 'scrollFollowSetting' + box.attr( 'id' ), false, { expires: 365, path: '/'} );
+						},
+						function ()
+						{
+							isActive = true;
+							
+							$( this ).text( options.onText );
+							
+							$.cookie( 'scrollFollowSetting' + box.attr( 'id' ), true, { expires: 365, path: '/'} );
+							
+							ani();
 						}
-						
-						$.cookie( 'scrollFollowSetting' + box.attr( 'id' ), false, { expires: 365, path: '/'} );
-					}
-				);
-		}
-		else
-		{
-			$( '#' + options.killSwitch ).text( options.onText )
-				.toggle( 
-					function ()
-					{
-						isActive = false;
-						
-						$( this ).text( options.offText );
-						
-						box.animate(
-							{
-								top: initialTop
-							}, 0
-						);	
-						
-						$.cookie( 'scrollFollowSetting' + box.attr( 'id' ), false, { expires: 365, path: '/'} );
-					},
-					function ()
-					{
-						isActive = true;
-						
-						$( this ).text( options.onText );
-						
-						$.cookie( 'scrollFollowSetting' + box.attr( 'id' ), true, { expires: 365, path: '/'} );
-						
-						ani( box, initialTop, boxInitialOffsetTop, paddingAdjustment );
-					}
-				);
+					);
+			}
 		}
 		
 		// If no parent ID was specified, and the immediate parent does not have an ID
 		// options.container will be undefined. So we need to figure out the parent element.
-		var cont;
-		
 		if ( options.container == '')
 		{
-			cont = box.parent();
+			box.cont = box.parent();
 		}
 		else
 		{
-			cont = $( '#' + options.container );
+			box.cont = $( '#' + options.container );
 		}
 		
-		// Finds the defaul positioning of the box.
-		var contTop = parseInt( cont.offset().top );
-		var boxInitialOffsetTop =  parseInt( box.offset().top );
-		var initialTop = parseInt( box.css( 'top' ) ) || 0;
+		// Finds the default positioning of the box.
+		box.initialOffsetTop =  parseInt( box.offset().top );
+		box.initialTop = parseInt( box.css( 'top' ) ) || 0;
 		
-
 		// Hack to fix different treatment of boxes positioned 'absolute' and 'relative'
-		var paddingAdjustment;
 		if ( box.css( 'position' ) == 'relative' )
 		{
-			paddingAdjustment = parseInt( cont.css( 'paddingTop' ) ) + parseInt( cont.css( 'paddingBottom' ) );
+			box.paddingAdjustment = parseInt( box.cont.css( 'paddingTop' ) ) + parseInt( box.cont.css( 'paddingBottom' ) );
 		}
 		else
 		{
-			paddingAdjustment = 0;
+			box.paddingAdjustment = 0;
 		}
 		
-		// Animated the box when the page is scrolled.
+		// Animate the box when the page is scrolled
 		$( window ).scroll( function ()
 			{
-				ani( box, initialTop, boxInitialOffsetTop, paddingAdjustment );
+				// Sets up the delay of the animation
+				$.fn.scrollFollow.interval = setTimeout( function(){ ani();} , options.delay );
+				
+				// To check against right before setting the animation
+				box.lastScroll = new Date().getTime();
+			}
+		);
+		
+		// Animate the box when the page is resized
+		$( window ).resize( function ()
+			{
+				// Sets up the delay of the animation
+				$.fn.scrollFollow.interval = setTimeout( function(){ ani();} , options.delay );
+				
+				// To check against right before setting the animation
+				box.lastScroll = new Date().getTime();
 			}
 		);
 
-		ani( box, initialTop, boxInitialOffsetTop, paddingAdjustment );
+		// Run an initial animation on page load
+		box.lastScroll = 0;
+		
+		ani();
 	};
 	
 	$.fn.scrollFollow = function ( options )
@@ -244,12 +230,12 @@
 		options.relativeTo = options.relativeTo || 'top';
 		options.speed = options.speed || 500;
 		options.offset = options.offset || 0;
-		options.easing = options.easing || 'easeOutBack';
+		options.easing = options.easing || 'swing';
 		options.container = options.container || this.parent().attr( 'id' );
 		options.killSwitch = options.killSwitch || 'killSwitch';
 		options.onText = options.onText || 'Turn Slide Off';
 		options.offText = options.offText || 'Turn Slide On';
-		options.animate = options.animate || 'yes';
+		options.delay = options.delay || 0;
 		
 		this.each( function() 
 			{
@@ -260,4 +246,6 @@
 		return this;
 	};
 })( jQuery );
+
+
 
